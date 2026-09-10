d759f877f185ad74588784ad7280ef2ea5c5d4a1
Mohsen Shabanian <mohsen1299@gmail.com>
2026-09-10 17:59:17 +0330

    step 2 of learning variable is complete

--- step02/main.go
+++ step02/main.go
@@ -4,10 +4,22 @@ import "fmt"
 
 func main()  {
 	var name string = "Mohsen"
-	var age int = 40
+	var age int = 30
+	var exp int = 5
 	var developer bool = true
-	
-	fmt.Println(name)
-    fmt.Println(age)
-    fmt.Println(developer)
+	var salary float64 = 5000.50
+
+	const language = "GO"
+
+	fmt.Println("========================")
+	fmt.Println("     GO DEVELOPER")
+	fmt.Println("========================")
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Experience:", exp)
+	fmt.Println("Backend Developer:", developer)
+	fmt.Println("Salary:", salary)
+	fmt.Println("Language:", language)
+	fmt.Println("Years until 40:", (40-age))
+	fmt.Println("========================")
 }
