c0e4a4736f98d99913ec4ea3675a39d945a4e07e
Mohsen Shabanian <mohsen1299@gmail.com>
2026-09-10 17:43:44 +0330

    task 1 is complete

--- step01/main.go
+++ step01/main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hello World!")
+	fmt.Println("========================")
+	fmt.Println("\tGO LEARNING")
+	fmt.Println("========================")
+	fmt.Println("Name: Mohsen")
+	fmt.Println("Role: Backend Developer")
+	fmt.Println("Language: Go")
+	fmt.Println("========================")				
 }
