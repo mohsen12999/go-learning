b478f041b1a27e2afcdaffacb49be090eec3c36b
Mohsen Shabanian <mohsen1299@gmail.com>
2026-09-10 17:48:02 +0330

    start step 2 for learning variable

---
+++ step02/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var name string = "Mohsen"
+	var age int = 40
+	var developer bool = true
+	
+	fmt.Println(name)
+    fmt.Println(age)
+    fmt.Println(developer)
+}
